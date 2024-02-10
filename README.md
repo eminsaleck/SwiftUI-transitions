6898bf50716e71c344528ab3ccc50f395353cdce
Emin Saleck <emin.saleck@poly-tech.com.ua>
2024-02-10 14:55:31 +0000

    added Drop transition

--- README.md
+++ README.md
@@ -5,15 +5,18 @@
 <p>Dive into our showcase of transitions, each designed to enrich your user interface with smooth, engaging animations.</p>
 
 <h3>Paper Transition</h3>
-<p>Watch as views fold and unfold like paper with Paper Transition, adding a three-dimensional flair to your app's navigation.</p>
 <div style="text-align:center; margin-bottom:20px;">
   <img src="https://github.com/eminsaleck/SwiftUI-transitions/blob/f786469c578c67f134b401216e5d207654076ef8/Sources/Transitions/examples_gifs/paper.gif" width="200px" />
 </div>
 
 <h3>Erase Transition</h3>
-<p>Reveal content with the swipe of a digital eraser, making elements disappear or appear on the screen in a uniquely interactive manner.</p>
 <div style="text-align:center; margin-bottom:20px;">
   <img src="https://github.com/eminsaleck/SwiftUI-transitions/blob/e4676a6c4315b452c1642395606d01386a685809/Sources/Transitions/examples_gifs/erase.gif" width="200px" />
+</div>
+
+<h3>Drop Transition</h3>
+<div style="text-align:center; margin-bottom:20px;">
+  <img src="https://github.com/eminsaleck/SwiftUI-transitions/blob/e4676a6c4315b452c1642395606d01386a685809/Sources/Transitions/examples_gifs/drop.gif" width="200px" />
 </div>
 
 <h2>Getting Started</h2>
---
+++ Sources/Transitions/DropTransition.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+public struct DropTransition: Transition {
+    public var isBlurred: Bool
+    
+    public init(isBlurred: Bool = true) {
+        self.isBlurred = isBlurred
+    }
+    
+    public func body(content: Content, phase: TransitionPhase) -> some View {
+        content
+          .opacity(phase.isIdentity ? 1.0 : 0.0)
+          .offset(y: phase.offset)
+          .blur(radius: isBlurred == true ? phase.blur : 0)
+    }
+}
+
+fileprivate extension TransitionPhase {
+    var blur: CGFloat {
+        switch self {
+        case .willAppear: return 3.5
+        case .identity: return .zero
+        case .didDisappear: return 59
+        }
+    }
+    
+    var offset: CGFloat {
+        switch self {
+        case .willAppear: return 500
+        case .identity: return .zero
+        case .didDisappear: return 500
+        }
+    }
+    
+    var ofsset: CGFloat {
+        switch self {
+        case .willAppear: return 500
+        case .identity: return .zero
+        case .didDisappear: return 500
+        }
+    }
+    
+    var rotation: Angle {
+        switch self {
+        case .willAppear: return .degrees(30)
+        case .identity: return .zero
+        case .didDisappear: return .degrees(-30)
+        }
+    }
+}
+
